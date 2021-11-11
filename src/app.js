1500c57072329ce8b2cc32d131434409b30789f9
Hiya Madan <madan@Dell-Hiya.(none)>
2021-11-11 17:55:26 +0530

    initial Commit

---
+++ src/app.js
@@ -0,0 +1,113 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs')
+const geocode=require('./utils/geocode')
+const forecast=require('./utils/forecast')
+
+const app =  express()
+
+//Define paths for express config
+const publicDirectoryPath = path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
+
+//Setup handlebars engine and views location
+app.set('views',viewsPath)
+app.set('view engine','hbs')
+hbs.registerPartials(partialsPath)
+
+//Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('/',(req,res)=>{
+    res.render('index',{
+        title:'Weather',
+        name:'Hiya Madan'
+    })
+})
+
+app.get('/weather',(req,res)=>{
+    if(!req.query.address){
+        return res.send({
+             error:'You must provide an address'
+         })
+
+    }
+    geocode(req.query.address, (error, { latitude, longitude, location }={}) => {
+        if (error) {
+            return res.send({error})
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({error})
+            }
+            res.send({
+                location:location,
+                forecast:forecastData,
+                address:req.query.address
+            })
+        })
+    })
+
+
+
+
+
+
+
+// res.send({
+//     forecast:"Its 30 deg outside",
+//     location:"India",
+//     address:req.query.address
+// })
+
+})
+
+app.get('/products',(req,res)=>{
+    if(!req.query.address){
+       return res.send({
+            error:'You must provide a search term'
+        })
+    }
+    console.log(req.query);
+    res.send({
+        products:[]
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:'About me',
+        name:'Hiya Madan'
+    })
+})
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        title:'Help',
+        name: 'Hiya Madan',
+        message:'Hey,I am here to help'
+    })
+})
+
+
+
+app.get('/help/*',(req,res)=>{
+    res.render('404',{
+        title:"404",
+        name:"Hiya Madan",
+        errorMessage:'Help Article not found.'
+    })
+})
+
+app.get('*',(req,res)=>{
+    res.render('404',{
+        title:"404",
+        name:"Hiya Madan",
+        errorMessage:'Page not Found!'
+    })
+})
+app.listen(3000,()=>{
+    console.log('Server started on port 3000.');
+})
---
+++ src/utils/geocode.js
@@ -0,0 +1,23 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    // const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?types=country&access_token=pk.eyJ1IjoiaGl5YW1hZGFuIiwiYSI6ImNrdnBpMGFwNzc3eGMycG1za2xtb2p2eTIifQ.MveFKDFp523vFOR90ueitw'
+
+    request({ url, json: true }, (error, { body }) => {
+        // console.log(body);
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (body.features.length === 0) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            callback(undefined, {
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
